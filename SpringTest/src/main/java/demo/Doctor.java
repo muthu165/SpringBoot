7d9b3c26ada6eb1396def8eb91b6abc9389f6101
Muthukrishnan <muthu165@gmail.com>
2024-12-15 13:11:59 +0530

    aware interfaces to custom bean life cycle BeanNameAware PostConstruct PreDestroy

--- SpringTest/src/main/java/demo/Doctor.java
+++ SpringTest/src/main/java/demo/Doctor.java
@@ -1,11 +1,15 @@
 package demo;
 
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope(scopeName = "prototype")
-public class Doctor implements Staff{
+public class Doctor implements Staff, BeanNameAware {
     @Override
     public String toString() {
         return "Doctor{" +
@@ -28,5 +32,20 @@ public class Doctor implements Staff{
 
     public void setQualification(String qualification) {
         this.qualification = qualification;
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("Set bean name method is called");
+    }
+
+    @PostConstruct
+    public void postConstruct(){
+        System.out.println("PostConstruct method is called");
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        System.out.println("PreDestroy method is called");
     }
 }
