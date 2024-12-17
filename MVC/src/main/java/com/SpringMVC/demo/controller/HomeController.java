69f6a46b73aad05c6b5047c81ae29effc0abcc7e
Muthukrishnan <muthu165@gmail.com>
2024-12-17 12:53:43 +0530

    creating different profiles for dev, qa, prod

--- MVC/src/main/java/com/SpringMVC/demo/controller/HomeController.java
+++ MVC/src/main/java/com/SpringMVC/demo/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.SpringMVC.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+    @Value("${home.message}")
+    private String homeMessage;
+
 //    @RequestMapping(path = "/",method = RequestMethod.GET)
     @GetMapping("/")
     public String homePage(){
-        return "Welcome to Home";
+        return homeMessage;
     }
 }
